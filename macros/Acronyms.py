19550f1101b63dc9ad4f1b6662c4c025f647e01e
IIDelta <jaustind1@gmail.com>
2025-03-21 10:52:55 -0400

    necessary changes

--- macros/Acronyms.py
+++ macros/Acronyms.py
@@ -12,19 +12,24 @@ def find_acronyms(word_app, definition_path, context_range=5):
     unlikely_pattern = re.compile(r'^.*[\u0391-\u03A9A-Z]{2,}.*$')
     consecutive_numbers_pattern = re.compile(r'\d{3,}')
     all_digits_or_hyphens_pattern = re.compile(r'^[\d-]+$')
-    
     # Exclude and include lists
     exclude = {
-        'ON', 'BC', 'MB', 'NB', 'NL', 'NS', 'NT', 'NU', 'PE', 'QC', 'SK', 'YT', 'AL', 'AK', 'AZ', 'AR', 'CA',
-        'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI',
-        'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI',
-        'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY', 'Jan', 'Feb', 'Mar', 'Apr', 'May',
-        'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'MM', 'DD', 'YYYY', "MMM"
+        'ON', 'BC', 'MB', 'NB', 'NL', 'NS', 'NT', 'NU',
+        'PE', 'QC', 'SK', 'YT', 'AL', 'AK', 'AZ', 'AR', 'CA',
+        'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN',
+        'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI',
+        'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM',
+        'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI',
+        'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV',
+        'WI', 'WY', 'Jan', 'Feb', 'Mar', 'Apr', 'May',
+        'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'MM',
+        'DD', 'YYYY', "MMM"
     }
     include = {'fl oz'}
 
     with open(definition_path, 'r') as f:
-        defined_acronyms = {line.split('\t')[0] for line in f.read().splitlines()}
+        defined_acronyms = {
+            line.split('\t')[0] for line in f.read().splitlines()}
 
     doc = word_app.ActiveDocument
     text = ' '.join([p.Range.Text for p in doc.Paragraphs])
@@ -35,34 +40,46 @@ def find_acronyms(word_app, definition_path, context_range=5):
     prev_word_acronym_index = None
 
     for i, word in enumerate(word_list):
-        if prev_word_acronym_index is not None and i == prev_word_acronym_index + 1:
+        if prev_word_acronym_index\
+         is not None and i == prev_word_acronym_index + 1:
             prev_word_acronym_index = None
             continue
-        elif i < len(word_list) - 1 and re.match(r'^[A-Z]+\d+$', word) and re.match(r'^\d+[A-Z]+$', word_list[i + 1]):
+        elif i < len(word_list) - 1 and re.match(r'^[A-Z]+\d+$', word) and\
+                re.match(r'^\d+[A-Z]+$', word_list[i + 1]):
             prev_word_acronym_index = i
             continue
 
         if word in exclude or consecutive_numbers_pattern.search(word) or \
-            all_digits_or_hyphens_pattern.match(word) or hyphenated_pattern.match(word) or \
-            multiple_periods_pattern.match(word):
+            all_digits_or_hyphens_pattern.match(word) or\
+            hyphenated_pattern.match(word) or\
+                multiple_periods_pattern.match(word):
             continue
 
         if word[-1].isdigit() and word[:-1] in defined_acronyms:
-            acronyms['likely'][word[:-1]] = get_context(i, word_list, context_range)
+            acronyms['likely'][word[:-1]] = get_context(
+                i, word_list, context_range)
         elif word[-1].lower() == "s" and word[:-1] in defined_acronyms:
-            acronyms['likely'][word[:-1]] = get_context(i, word_list, context_range)
+            acronyms['likely'][word[:-1]] = get_context(
+                i, word_list, context_range)
         elif word in defined_acronyms:
-            acronyms['likely'][word] = get_context(i, word_list, context_range)
-        elif likely_pattern.match(word) and not is_english_word(word.lower()):
-            acronyms['likely'][word] = get_context(i, word_list, context_range)
-        elif possible_pattern.match(word) and not is_english_word(word.lower()):
-            acronyms['possible'][word] = get_context(i, word_list, context_range)
+            acronyms['likely'][word] = get_context(
+                i, word_list, context_range)
+        elif likely_pattern.match(word) and not\
+                is_english_word(word.lower()):
+            acronyms['likely'][word] = get_context(
+                i, word_list, context_range)
+        elif possible_pattern.match(word) and not\
+                is_english_word(word.lower()):
+            acronyms['possible'][word] = get_context(
+                i, word_list, context_range)
         elif word.lower() == 'vitamin':
             continue
         elif unlikely_pattern.match(word) and word not in defined_acronyms:
-            acronyms['unlikely'][word] = get_context(i, word_list, context_range)
+            acronyms['unlikely'][word] = get_context(
+                i, word_list, context_range)
 
-        if len(word) in {1, 2} and word.isupper() and i > 0 and i < len(word_list) - 1:
+        if len(word) in {1, 2} and word.isupper() and\
+                i > 0 and i < len(word_list) - 1:
             if word_list[i - 1][0].isupper() and word_list[i + 1][0].isupper():
                 for category in acronyms:
                     if word in acronyms[category]:
@@ -75,20 +92,27 @@ def find_acronyms(word_app, definition_path, context_range=5):
 
     for phrase in include:
         if phrase in text and phrase in defined_acronyms:
-            acronyms['likely'][phrase] = get_context(text.index(phrase), word_list, context_range)
+            acronyms['likely'][phrase] = get_context(
+                text.index(phrase), word_list, context_range)
 
     return {
-        'likely': {key: acronyms['likely'][key] for key in sorted(acronyms['likely'])},
-        'possible': {key: acronyms['possible'][key] for key in sorted(acronyms['possible'])},
-        'unlikely': {key: acronyms['unlikely'][key] for key in sorted(acronyms['unlikely'])},
+        'likely': {key: acronyms['likely'][key] for key in sorted(
+            acronyms['likely'])},
+        'possible': {key: acronyms['possible'][key] for key in sorted(
+            acronyms['possible'])},
+        'unlikely': {key: acronyms['unlikely'][key] for key in sorted(
+            acronyms['unlikely'])},
     }
 
-# Other helper functions like get_context, get_definition, is_english_word remain the same
 
+# Other helper functions like get_context,
+# get_definition, is_english_word remain the same
 def get_context(index, word_list, range):
     start = max(0, index - range)
-    end = min(len(word_list), index + range + 1)  # Adjust end to include the word at the current index
+    end = min(len(word_list), index + range + 1)
+    # Adjust end to include the word at the current index
     return ' '.join(word_list[start:end])
+
 
 def get_definition(acronym, definition_path):
     definitions = {}
@@ -97,6 +121,7 @@ def get_definition(acronym, definition_path):
             acronym_definition, definition = line.strip().split("\t")
             definitions[acronym_definition] = definition
     return definitions.get(acronym, "")
+
 
 def is_english_word(word):
     if wordnet.synsets(word):
--- ui/replacevalues_selectionwindow.py
+++ ui/replacevalues_selectionwindow.py
@@ -1,14 +1,15 @@
 # ui/replacevalues_selectionwindow.py
 
 from PyQt5.QtWidgets import (QMainWindow, QVBoxLayout, QCheckBox,
-                            QWidget, QPushButton, QLabel, QFileDialog,
-                            QTextEdit)
+                             QWidget, QPushButton, QLabel, QFileDialog,
+                             QTextEdit)
 import os
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import Qt, QTimer
 
 
 from macros.ReplaceValues_Selection import Macro_ReplaceValues_Selection
+
 
 class WildcardsInfoWindow(QMainWindow):
     def __init__(self, parent=None):
@@ -59,7 +60,7 @@ class ReplaceValuesSelectionWindow(QMainWindow):
         # Build the full icon path
         icon_path = os.path.join(dir_path, "leaf.png")
         # Set the window icon
-        self.setWindowIcon(QIcon(icon_path))  
+        self.setWindowIcon(QIcon(icon_path))
         self.layout = QVBoxLayout()
 
         # Update the window flags based on the parent's checkbox state
@@ -80,16 +81,14 @@ class ReplaceValuesSelectionWindow(QMainWindow):
         """)
         self.excel_file_button.clicked.connect(self.choose_excel_file)
         self.layout.addWidget(self.excel_file_button)
-        
         self.excel_file_label = QLabel("", self)  # Create the label
         self.layout.addWidget(self.excel_file_label)
 
         # Add the checkbox for enabling wildcards
         self.use_wildcards_checkbox = QCheckBox("Use Wildcards", self)
         self.layout.addWidget(self.use_wildcards_checkbox)
-        self.use_wildcards_checkbox.stateChanged.connect(self.toggle_wildcards_info_window)
-
-
+        self.use_wildcards_checkbox.stateChanged.connect(
+            self.toggle_wildcards_info_window)
         self.run_macro_button = QPushButton("Run Macro", self)
         self.run_macro_button.setStyleSheet("""
             QPushButton {
@@ -107,7 +106,7 @@ class ReplaceValuesSelectionWindow(QMainWindow):
         self.widget.setLayout(self.layout)
         self.setCentralWidget(self.widget)
         self.layout.addStretch()
-        
+
     def toggle_wildcards_info_window(self, checked):
         if checked:
             self.wildcards_info_window = WildcardsInfoWindow(self)
@@ -119,26 +118,25 @@ class ReplaceValuesSelectionWindow(QMainWindow):
 
     def choose_excel_file(self):
         options = QFileDialog.Options()
-        self.excel_file, _ = QFileDialog.getOpenFileName(self, "Choose an Excel File", 
-                                            "", "Excel Files (*.xlsx)", options=options)
+        self.excel_file, _ = QFileDialog.getOpenFileName(
+            self, "Choose an Excel File", "",
+            "Excel Files (*.xlsx)", options=options)
         if self.excel_file:
-            self.excel_file_label.setText("Excel File: " \
-                                        + os.path.basename(self.excel_file))
+            self.excel_file_label.setText(
+                "Excel File: " + os.path.basename(self.excel_file))
 
     def run_macro(self):
         try:
             if not self.excel_file:
                 print("No Excel file chosen.")
                 return
-
             macro = Macro_ReplaceValues_Selection()
             macro.load_excel_file(self.excel_file)
             use_wildcards = self.use_wildcards_checkbox.isChecked()
             macro.replace_values(use_wildcards)
             macro.save_document()
-
             print("Replacements made in selected text.")
-        
+
         except Exception as e:
             print(str(e))
             self.parent().label.setText(f"Error: {str(e)}")
